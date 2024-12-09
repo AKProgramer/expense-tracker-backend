281a4ef06a8d9f6c140d09cc21209e046ca2e98a
Ali Hassan <i221545@nu.edu.pk>
2024-12-09 18:00:30 +0500

    ma ghar ja raha jo hostle

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: process.env.FRONTEND_URL || '*',
+app.use(cors({ origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
    credentials: true }));
 
 connect();
-
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}`);
+  console.log(`Request Headers:`, req.headers);
+  console.log(`Request Body:`, req.body);
+  next();
+});
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
