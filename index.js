0fe78f2562a61ef659072b04cf82615eb17039ee
Ali Hassan <i221545@nu.edu.pk>
2024-12-09 16:24:13 +0500

    last commit

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const connect = require('./database/connectDB');
 const userRoutes = require('./routes/userRoutes');
 const groupRoutes = require('./routes/groupRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 const approvalRoutes = require('./routes/approvalRoutes');
-const cors = require('cors')
+const cors = require('cors');
 require('colors');
 require('dotenv').config();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    cors({
-      origin: process.env.FRONTEND_URL, // Replace with your frontend's URL
-      credentials: true, // Allow cookies and other credentials
-    })
-  );
- 
+  cors({
+    origin: process.env.FRONTEND_URL, // Replace with your frontend's URL
+    credentials: true, // Allow cookies and other credentials
+  })
+);
+
 connect();
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+  res.send('Hello World');
+});
+
 app.use('/api/users', userRoutes);
 app.use('/api/groups', groupRoutes);
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/approvals', approvalRoutes);
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`.yellow.bold);
-})
+
+// Export the app as a Vercel function handler
+module.exports = (req, res) => {
+  app(req, res); // Vercel will handle each request through this handler
+};
